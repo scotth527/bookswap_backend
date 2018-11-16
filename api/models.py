0726a43d8526bbd4c8de512d6a302518cdd79587
Scott Huang <scottyh527@gmail.com>
2018-11-16 23:42:40 +0000

    added more models

--- api/models.py
+++ api/models.py
@@ -1,13 +1,54 @@
 from rest_framework import serializers
 from django.db import models
+from django.conf import settings
 
 # Create your models here. 
-class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
     
+class Profile(models.Model):
+    address = models.CharField(max_length=50)
+    birthday = models.CharField(max_length=50)
+    favorite_genre = models.CharField(max_length=50)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    
+class Books(models.Model):
+    Goodreads_ID = models.CharField(max_length=50)
+    ID = models.CharField(max_length=50)
+    Title = models.CharField(max_length=50)
+    Author = models.CharField(max_length=50)
+    Genre = models.CharField(max_length=50)
+    Language = models.CharField(max_length=50)
+    Description = models.CharField(max_length=1000)
 
-class ContactSerializer(serializers.ModelSerializer):
+    
+class Wishlist(models.Model):
+    book_id = models.ForeignKey('Books', on_delete=models.CASCADE)
+
+
+class PersonalInventory(models.Model):
+    book_id = models.ForeignKey('Books', on_delete=models.CASCADE)
+    
+        
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Contact
+        model = Profile
         exclude = ()
+        
+class WishlistSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Wishlist
+        exclude = ()
+        
+class PersonalInventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PersonalInventory
+        exclude = ()
+        
+class BooksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Books
+        exclude = ()
+        
+        
--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,22 @@ from django.urls import path, include
 from api import views
 from rest_framework_jwt.views import obtain_jwt_token
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   
+   public=True,
+   
+)
+
 urlpatterns = [
-    path('contacts/<int:contact_id>', views.ContactsView.as_view(), name='id-contacts'),
+    path('users/<int:users_id>', views.UsersView.as_view(), name='id-users'),
     path('contacts/', views.ContactsView.as_view(), name='all-contacts'),
     path('login/', obtain_jwt_token)
 ]
