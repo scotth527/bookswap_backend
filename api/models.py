a87dae1e1d09e3977b71d9f1a3bf16f31438fb66
Scott Huang <scottyh527@gmail.com>
2018-11-22 01:35:58 +0000

    added fixture

--- api/models.py
+++ api/models.py
@@ -14,6 +14,8 @@ class Books(models.Model):
     
 
 class Profile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     birthday = models.CharField(max_length=50)
     favorite_genre = models.CharField(max_length=50)
